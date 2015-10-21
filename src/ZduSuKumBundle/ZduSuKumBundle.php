09a26786121b640e18cd641926309a31af2a230e
Karol Zdunczuk <zdunczukowsky@gmail.com>
2015-10-21 15:01:15 +0200

    Karol

---
+++ src/ZduSuKumBundle/ZduSuKumBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ZduSuKumBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ZduSuKumBundle extends Bundle
+{
+}
--- web/app.php
+++ web/app.php
@@ -1,7 +1,5 @@
 <?php
 
-// Siema
-
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
